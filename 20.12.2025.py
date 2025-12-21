c7fc45794aaeda9dedd3a258419036f4fb350a87
Grecha-code <alex.pashkov2012@gmail.com>
2025-12-21 21:41:46 +0700

    20.12.2025.py

--- 20.12.2025.py
+++ 20.12.2025.py
@@ -1,8 +1,6 @@
 import time
 
-
 def create_hanoi(n, start, end, spare):
-    global towers, moves
     moves = 0
     qq = {
         start: list(range(n, 0, -1)),
@@ -22,9 +20,14 @@ def create_hanoi(n, start, end, spare):
     time.sleep(2)
 
     def move_disk(from_tower, to_tower):
+        nonlocal moves
         disk = towers[from_tower].pop()
         towers[to_tower].append(disk)
+        print()
+        print(f"Ход номер {moves+1}:")
         print(f"\nПеремещаем диск {disk} с {from_tower} на {to_tower}")
+        moves += 1
+        print_towers(towers)
 
     def hanoi(n, start, end, spare):
         if n == 1:
@@ -32,29 +35,29 @@ def create_hanoi(n, start, end, spare):
             return
 
         hanoi(n-1, start, spare, end)
-        print_towers(towers)
 
         move_disk(start, end)
-        print_towers(towers)
 
         hanoi(n-1, spare, end, start)
-        print_towers(towers)
 
     hanoi(n, start, end, spare)
 
+    for i in range(3):
+        print()
     print("Окончательное состояние: ")
     print_towers(towers)
+    print(f"Понадобилось ходов: {moves}")
 
 
 if __name__ == "__main__":
     n = int(input("Введите количество дисков: "))
     start = int(input("Введите начальный стержень дисков: "))
     end = int(input("Введите конечный стержень дисков: "))
-    spare = None
+    start_time = time.perf_counter()
     my_list = [1, 2, 3]
     my_list.remove(start)
     my_list.remove(end)
-    for i in range(len(my_list)):
-        spare = my_list[i]
-
+    spare = my_list[0]
     create_hanoi(n, start, end, spare)
+    end_time = time.perf_counter()
+    print(f"Время выполнения {end_time - start_time - 4: 0.6f} сек")
